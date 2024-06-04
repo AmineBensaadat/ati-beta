e52c53a4782cad38ed2ab9872fe24926dd894dc2
bensaadat <bensaadat.amine@gmail.com>
2024-06-04 15:40:47 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-gray-200 p-4">
+      <h1 className="text-2xl font-bold text-center text-indigo-800">
+        Hello, Tailwind CSS!
+      </h1>
+      <p className="text-center text-gray-600 mt-4">
+        This is a React project with Tailwind CSS.
+      </p>
+    </div>
+  );
+}
+
+export default App;
